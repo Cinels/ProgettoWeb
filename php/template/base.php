f07c35c113c6bfa2c852bd60f1a06314621bd1cc
Lorenzo Cinelli <lorenzo.cinelli.it@gmail.com>
2025-05-28 18:49:14 +0200

    Added path costants

--- php/template/base.php
+++ php/template/base.php
@@ -1,3 +1,4 @@
+<?php require("../utils/utils.php"); ?>
 <!DOCTYPE html>
 <html lang="it">
     <head>
@@ -6,28 +7,27 @@
     </head>
     <body>
         <header>
-            <a href="../pages/index.php"><img src="../../resources/logo.png" alt="Home Page"/></a>
+            <a href="<?php echo PAGES_DIR ?>index.php"><img src="<?php echo RESOURCES_DIR ?>logo.png" alt="Home Page"/></a>
 
             <label>
-                <img src="../../resources/search.png" alt="Search"/>
+                <img src="<?php echo RESOURCES_DIR ?>search.png" alt="Search"/>
                 <input type="text" name="search"/>
             </label>
 
-            <!-- varie opzioni in base al login -->
             <?php if(false/* no login */): ?>
-                <a href="../pages/login.php"><img src="../../resources/favourite.png" alt="Favourite List"/></a>
-                <a href="../pages/login.php"><img src="../../resources/cart.png" alt="Shopping Cart"/></a>
-                <a href="../pages/login.php"><img src="../../resources/orders.png" alt="My Orders"/></a>
-                <a href="../pages/login.php"><img src="../../resources/profile.png" alt="My Profile"/></a>
+                <a href="<?php echo PAGES_DIR ?>login.php"><img src="<?php echo RESOURCES_DIR ?>favourite.png" alt="Favourite List"/></a>
+                <a href="<?php echo PAGES_DIR ?>login.php"><img src="<?php echo RESOURCES_DIR ?>cart.png" alt="Shopping Cart"/></a>
+                <a href="<?php echo PAGES_DIR ?>login.php"><img src="<?php echo RESOURCES_DIR ?>orders.png" alt="My Orders"/></a>
+                <a href="<?php echo PAGES_DIR ?>login.php"><img src="<?php echo RESOURCES_DIR ?>profile.png" alt="My Profile"/></a>
             <?php else: ?>
                 <?php if(true/* login cliente */): ?>
-                    <a href="../pages/favourite.php"><img src="../../resources/favourite.png" alt="Favourite List"/></a>
-                    <a href="../pages/cart.php"><img src="../../resources/cart.png" alt="Shopping Cart"/></a>
+                    <a href="<?php echo PAGES_DIR ?>favourite.php"><img src="<?php echo RESOURCES_DIR ?>favourite.png" alt="Favourite List"/></a>
+                    <a href="<?php echo PAGES_DIR ?>cart.php"><img src="<?php echo RESOURCES_DIR ?>cart.png" alt="Shopping Cart"/></a>
                 <?php else: ?>
-                    <a href="../pages/add_product.php"><img src="../../resources/add.png" alt="Add Product"/></a>
+                    <a href="<?php echo PAGES_DIR ?>add_product.php"><img src="<?php echo RESOURCES_DIR ?>add.png" alt="Add Product"/></a>
                 <?php endif; ?>
-                <a href="../pages/orders.php"><img src="../../resources/orders.png" alt="My Orders"/></a>
-                <a href="../pages/profile.php"><img src="../../resources/profile.png" alt="My Profile"/></a>
+                <a href="<?php echo PAGES_DIR ?>orders.php"><img src="<?php echo RESOURCES_DIR ?>orders.png" alt="My Orders"/></a>
+                <a href="<?php echo PAGES_DIR ?>profile.php"><img src="<?php echo RESOURCES_DIR ?>profile.png" alt="My Profile"/></a>
             <?php endif; ?>
         </header>
         
