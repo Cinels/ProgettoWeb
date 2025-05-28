41385216ac05ef76973ee121f66eb7759fbaa08e
Lorenzo Cinelli <lorenzo.cinelli.it@gmail.com>
2025-05-28 12:32:48 +0200

    Added base php page

---
+++ php/template/base.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <title>titolo</title>
+        <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    </head>
+    <body>
+        <header>
+            <!-- logo -->
+            <h1>Blog di Tecnologie Web</h1>
+        </header>
+        <footer>
+            <p>Tecnologie Web - A.A. 2024/2025</p>
+        </footer>
+
+    </body>
+</html>
