26e7077eea0177783d68b9a5f7beb0057cb510cd
Lorenzo Cinelli <lorenzo.cinelli.it@gmail.com>
2025-05-29 00:20:43 +0200

    Added home page main section

--- php/template/home.php
+++ php/template/home.php
@@ -1,3 +1,5 @@
 <section>
-    <h2>Pagina Principale</h2>
+    <a href="<?php echo PAGES_DIR ?>results.php"><img src="<?php echo RESOURCES_DIR ?>playstation.png" alt="Playstation"/></a>
+    <a href="<?php echo PAGES_DIR ?>results.php"><img src="<?php echo RESOURCES_DIR ?>switch.png" alt="Nintendo Switch"/></a>
+    <a href="<?php echo PAGES_DIR ?>results.php"><img src="<?php echo RESOURCES_DIR ?>xbox.png" alt="Xbox"/></a>
 </section>
