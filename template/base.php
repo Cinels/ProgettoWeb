903a25df311b6d51567e81b2c2a82baa400c573b
Lorenzo Cinelli <lorenzo.cinelli.it@gmail.com>
2025-06-03 16:25:20 +0200

    Fixed images paths

--- template/base.php
+++ template/base.php
@@ -7,27 +7,27 @@
     </head>
     <body>
         <header>
-            <a href="<?php echo PAGES_DIR ?>index.php"><img src="<?php echo RESOURCES_DIR ?>logo.png" alt="Pagina Principale"/></a>
+            <a href="<?php echo PAGES_DIR ?>index.php"><img src="<?php echo RESOURCES_DIR ?>header/logo.png" alt="Pagina Principale"/></a>
 
             <label>
-                <img src="<?php echo RESOURCES_DIR ?>search.png" alt="Cerca"/>
+                <img src="<?php echo RESOURCES_DIR ?>header/search.png" alt="Cerca"/>
                 <input type="text" name="search"/>
             </label>
 
             <?php if(false/* no login */): ?>
-                <a href="<?php echo PAGES_DIR ?>login.php"><img src="<?php echo RESOURCES_DIR ?>cuore.png" alt="Lista Preferiti"/></a>
-                <a href="<?php echo PAGES_DIR ?>login.php"><img src="<?php echo RESOURCES_DIR ?>carrello.png" alt="Carrello"/></a>
-                <a href="<?php echo PAGES_DIR ?>login.php"><img src="<?php echo RESOURCES_DIR ?>ciamioncino.png" alt="Ordini"/></a>
-                <a href="<?php echo PAGES_DIR ?>login.php"><img src="<?php echo RESOURCES_DIR ?>utente.png" alt="Profilo"/></a>
+                <a href="<?php echo PAGES_DIR ?>login.php"><img src="<?php echo RESOURCES_DIR ?>header/cuore.png" alt="Lista Preferiti"/></a>
+                <a href="<?php echo PAGES_DIR ?>login.php"><img src="<?php echo RESOURCES_DIR ?>header/carrello.png" alt="Carrello"/></a>
+                <a href="<?php echo PAGES_DIR ?>login.php"><img src="<?php echo RESOURCES_DIR ?>header/ciamioncino.png" alt="Ordini"/></a>
+                <a href="<?php echo PAGES_DIR ?>login.php"><img src="<?php echo RESOURCES_DIR ?>header/utente.png" alt="Profilo"/></a>
             <?php else: ?>
                 <?php if(true/* login cliente */): ?>
-                    <a href="<?php echo PAGES_DIR ?>favourite.php"><img src="<?php echo RESOURCES_DIR ?>cuore.png" alt="Lista Preferiti"/></a>
-                    <a href="<?php echo PAGES_DIR ?>cart.php"><img src="<?php echo RESOURCES_DIR ?>carrello.png" alt="Carrello"/></a>
+                    <a href="<?php echo PAGES_DIR ?>favourite.php"><img src="<?php echo RESOURCES_DIR ?>header/cuore.png" alt="Lista Preferiti"/></a>
+                    <a href="<?php echo PAGES_DIR ?>cart.php"><img src="<?php echo RESOURCES_DIR ?>header/carrello.png" alt="Carrello"/></a>
                 <?php else: ?>
-                    <a href="<?php echo PAGES_DIR ?>add_product.php"><img src="<?php echo RESOURCES_DIR ?>add_T.png" alt="Aggiungi Prodotto"/></a>
+                    <a href="<?php echo PAGES_DIR ?>add_product.php"><img src="<?php echo RESOURCES_DIR ?>header/add_T.png" alt="Aggiungi Prodotto"/></a>
                 <?php endif; ?>
-                <a href="<?php echo PAGES_DIR ?>orders.php"><img src="<?php echo RESOURCES_DIR ?>ciamioncino.png" alt="Ordini"/></a>
-                <a href="<?php echo PAGES_DIR ?>profile.php"><img src="<?php echo RESOURCES_DIR ?>utente.png" alt="Profilo"/></a>
+                <a href="<?php echo PAGES_DIR ?>orders.php"><img src="<?php echo RESOURCES_DIR ?>header/ciamioncino.png" alt="Ordini"/></a>
+                <a href="<?php echo PAGES_DIR ?>profile.php"><img src="<?php echo RESOURCES_DIR ?>header/utente.png" alt="Profilo"/></a>
             <?php endif; ?>
         </header>
 
