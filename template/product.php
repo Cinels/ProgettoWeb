c78aac1f758d317063f670f92e50751890e9a4ab
Lorenzo Cinelli <lorenzo.cinelli.it@gmail.com>
2025-06-05 18:28:27 +0200

    Added favourite list and cart checks

--- template/product.php
+++ template/product.php
@@ -6,14 +6,18 @@
     
     
     <?php if($dbh->isLogged() && $dhb->getUserType() == "client"): 
-        if(true/* prodotto non nei preferiti */): ?>
-            <a href="#"><img src="<?php echo RESOURCES_DIR ?>cuore.png" alt="Aggiungi ai Preferiti"/></a>
+        if($dbh->isProductFavourite($result["idProdotto"])): ?>
+            <a href="<?php $dbh->addToFavourites($result["idProdotto"]) ?>"><img src="<?php echo RESOURCES_DIR ?>cuore.png" alt="Aggiungi ai Preferiti"/></a>
         <?php else: ?>
-            <a href="#"><img src="<?php echo RESOURCES_DIR ?>cuore_B.png" alt="Aggiungi ai Preferiti"/></a>
+            <a href="<?php $dbh->removeFromFavourites($result["idProdotto"]) ?>"><img src="<?php echo RESOURCES_DIR ?>cuore_B.png" alt="Rimuovi dai Preferiti"/></a>
         <?php endif;
     endif; ?>
     <p><?php echo $result["prezzo"] ?>€</p> <!-- gestisci sconto -->
-    <?php if($dbh->isLogged() && $dhb->getUserType() == "client"): ?>
-        <a href="#"><img src="<?php echo RESOURCES_DIR ?>carrello.png" alt="Aggiungi al Carrello"/></a>
-    <?php endif; ?>
+    <?php if($dbh->isLogged() && $dhb->getUserType() == "client"): 
+        if($dbh->isProductInCart($result["idProdotto"])): ?>
+            <a href="<?php $dbh->addToCart($result["idProdotto"], 1) ?>"><img src="<?php echo RESOURCES_DIR ?>carrello.png" alt="Aggiungi al Carrello"/></a>
+        <?php else: ?>
+            <a href="<?php $dbh->removeFromCart($result["idProdotto"]) ?>"><img src="<?php echo RESOURCES_DIR ?>carrello.png" alt="Rimuovi dal Carrello"/></a>
+        <?php endif;
+    php endif; ?>
 </section>
