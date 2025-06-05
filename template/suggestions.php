83a0c08bcfa52b27a373ab42109091b256ad3181
Lorenzo Cinelli <lorenzo.cinelli.it@gmail.com>
2025-06-05 19:41:46 +0200

    Added on sale management for suggestions

--- template/suggestions.php
+++ template/suggestions.php
@@ -5,7 +5,12 @@
                 <li><a src="#">
                     <img src="<?php echo $result["link"]?>" alt="Immagine Prodotto"/><br/>
                     <p><?php echo $result["nome"] ?></p>
-                    <p><?php echo $result["prezzo"] ?>€</p> <!-- gestisci sconto -->
+                    <?php if($result["sconto"] > 0) {
+                    $sale = $result["prezzo"] - $result["prezzo"]*$result["sconto"];
+                    echo "<ins>$sale</ins>€ <del>$result['prezzo']€</del>";
+                } else {
+                    echo "<p>$result['prezzo']€</p>";
+                }?>
                 </a></li>
             <?php endforeach;
         endif; ?>
