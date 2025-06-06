0b4b4b4187d0b393d90261fcc76986da3e17e4dc
Lorenzo Cinelli <lorenzo.cinelli.it@gmail.com>
2025-06-06 17:24:10 +0200

    Modified link section for suggested product

--- template/suggestions.php
+++ template/suggestions.php
@@ -3,14 +3,16 @@
         <?php if(isset($templateParams["results"])):
             foreach($templateParams["results"] as $result): ?>
                 <li><a href="#">
-                    <img src="<?php echo $result["link"]?>" alt="Immagine Prodotto"/><br/>
-                    <p><?php echo $result["nome"] ?></p>
-                    <?php if($result["offerta"] > 0) {
-                        $sale = $result["prezzo"] - $result["prezzo"]*($result["offerta"]/100);
-                        echo "<ins>".$sale."</ins> <del>".$result['prezzo']."</del>€";
-                    } else {
-                        echo "<p>".$result['prezzo']."€</p>";
-                    }?>
+                    <section>
+                        <img src="<?php echo $result["link"]?>" alt="Immagine Prodotto"/><br/>
+                        <p><?php echo $result["nome"] ?></p>
+                        <?php if($result["offerta"] > 0) {
+                            $sale = $result["prezzo"] - $result["prezzo"]*($result["offerta"]/100);
+                            echo "<ins>".$sale."</ins> <del>".$result['prezzo']."</del>€";
+                        } else {
+                            echo "<p>".$result['prezzo']."€</p>";
+                        }?>
+                    </section>
                 </a></li>
             <?php endforeach;
         endif; ?>
