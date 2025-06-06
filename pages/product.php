739dd54d657e80fbfe7f059631b22e478994b3dd
Lorenzo Cinelli <lorenzo.cinelli.it@gmail.com>
2025-06-06 22:47:32 +0200

    Fixed product page

--- pages/product.php
+++ pages/product.php
@@ -2,7 +2,12 @@
 require_once("../database/init.php");
 $templateParams["titolo"] = "Prodotto";
 $templateParams["main_content"] = ["product.php"];
-$templateParams["side_content"] = [$dbh->getRelatedProducts(10)];
+
+if(isset($_GET["search"])) {
+    $templateParams["results"] = $dbh->getProduct($_GET["search"]);
+    $templateParams["images"] = $dbh->getProductImages($_GET["search"]);
+    $templateParams["side_content"] = [$dbh->getRelatedProducts($_GET["search"], 10)];
+}
 
 require("../template/base.php")
 ?>
