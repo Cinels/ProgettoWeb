1b3a2c52fb9144b094ded1b0cf5b06daeec33edb
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-06-24 01:17:20 +0200

    bug fix

--- pages/results.php
+++ pages/results.php
@@ -2,13 +2,15 @@
 require_once("../database/init.php");
 $templateParams["titolo"] = "Risultati";
 $templateParams["main_content"] = ["results.php"];
-if(!($dbh->isLogged() && $dbh->getUserType() == "vendor")) {
-    $templateParams["side_content"] = [$dbh->getProductsOnSale(10), $dbh->getTrendProducts(10), $dbh->getYourInterestProducts(10)];
-}
 
 if(isset($_GET["search"])) {
     $templateParams["results"] = $dbh->getProductsFromResearch($_GET["search"]);
 }
+else if(!($dbh->isLogged() && $dbh->getUserType() == "vendor")) {
+    $templateParams["side_content"] = [$dbh->getProductsOnSale(10), $dbh->getTrendProducts(10), $dbh->getYourInterestProducts(10)];
+}
+
+
 
 require("../template/base.php")
 ?>
