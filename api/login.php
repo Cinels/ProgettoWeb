ef5f7bad9a2812eae3f7b17517ecf36b9cd5af5e
Lorenzo Cinelli <lorenzo.cinelli.it@gmail.com>
2025-07-01 12:41:54 +0200

    Added login javascript

---
+++ api/login.php
@@ -0,0 +1,21 @@
+<?php
+require_once("../database/init.php");
+
+if(isset($_POST["email"]) && isset($_POST["password"])) {
+    
+    $result = ['success' => false, 'message' => 'Si è verificato un errore sconosciuto.'];
+    if ($dbh->checkLogin($_POST["email"], $_POST["password"])) {
+        $result = ['success' => true, 'message' => 'Login avvenuto con successo!'];
+    } else {
+        $result = ['success' => false, 'message' => 'Email o password non validi.'];
+    }
+    header('Content-Type: application/json');
+    echo json_encode($result);
+
+    // if($dbh->checkLogin($_POST["email"], $_POST["password"])) {
+    //     header('Location:'.PAGES_DIR."index.php");
+    // } else {
+    //     $result["errorelogin"] = "Errore! Controllare username e password";
+    // }
+}
+?>
---
+++ js/login.js
@@ -0,0 +1,68 @@
+import * as paths from './paths.js';
+displayMainContent();
+
+function displayMainContent() {
+    const main = document.querySelector("main");
+    main.innerHTML += generateMainContent();
+
+    document.querySelector("main form").addEventListener("submit", function (event) {
+        event.preventDefault();
+        const email = document.querySelector("#email").value;
+        const password = document.querySelector("#password").value;
+        login(email, password);
+    });
+}
+
+function generateMainContent() {
+    let content = `
+        <section>
+            <h2>Accedi</h2>
+            <form action="#" method="POST">`;
+    // if(typeof input !== 'undefined') {
+        content += `<p></p>`;
+    // }
+    content += `<ul>
+                    <li>
+                        <label for="email">Email:</label><input type="email" id="email" name="email" required/>
+                    </li><li>
+                        <label for="password">Password:</label><input type="password" id="password" name="password" required/>
+                    </li><li>
+                        <input type="submit" name="submit" value="Accedi" />
+                    </li><li>
+                        <p>o</p>
+                    </li><li>
+                        <a href="${paths.PAGES_DIR}sign_in.php"><input type="button" value="Registrati" /></a>
+                    </li>
+                </ul>
+            </form>
+        </section>
+    `;
+    return content;
+}
+
+async function login(email, password) {
+    const url = `${paths.API_DIR}login.php`;
+    const formData = new FormData();
+    formData.append('email', email);
+    formData.append('password', password);
+    try {
+        const response = await fetch(url, {
+            method: "POST",                   
+            body: formData
+        });
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        console.log(json);
+        if(json["success"]) {
+            document.querySelector("form > p").innerText = "";
+            location.href = paths.PAGES_DIR + "index.php";
+        } else {
+            document.querySelector("form > p").innerText = json["message"];
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
