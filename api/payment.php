9a67228ae2c01131364acb51f7b6505cb960cd88
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-07-08 20:06:26 +0200

    add queries to notify caused by order

--- api/payment.php
+++ api/payment.php
@@ -3,12 +3,9 @@ require_once("../database/init.php");
 
 $totalPrice = 0.00;
 foreach ($dbh->getCart() as $product) {
-    if ($product["offerta"] > 0) {
-        $totalPrice += ($product["prezzo"] - $product["prezzo"]*($product["offerta"]/100)) * $product['quantita'];
-    } else {
-        $totalPrice += ($product['prezzo']) * $product['quantita'];
-    }
+        $totalPrice += ($product['prezzoScontato']) * $product['quantita'];
 }
+
 $result['total'] = $totalPrice;
 $result['num_products'] = count($dbh->getCart());
 $result['hasOrdered'] = false;
@@ -16,9 +13,20 @@ $result['hasOrdered'] = false;
 if(isset($_POST["card_number"]) && isset($_POST["expire_date"]) && isset($_POST["ccv"])){
     $result['hasOrdered'] = true;
     $dbh->createOrder("venditore@negozio.it");
+    $idOrder=$dbh->createOrder("venditore@negozio.it");
+    $soldout=$dbh->getSoldOutProductsNow($idOrder);
+    if(count($soldout) > 0) {
+        foreach($soldout as $exProd) {
+            $dbh->notifySoldoutProduct($exProd, "venditore@negozio.it");
+        }
+    }
+    $orderDetails=$dbh->getOrderDetails($idOrder);
+    foreach($orderDetails['idProdotto'] as $product) {
+        $dbh->manageLowQuantity($product, $dbh->getProduct($product)[0]['quantitaDisponibile']);
+    }
+    
 }
 
 header('Content-Type: application/json');
 echo json_encode($result);
-
 ?>
