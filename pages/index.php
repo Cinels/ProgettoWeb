1592f66afb0defa1d85c1120c8515ed9a60c20e8
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-07-09 18:41:06 +0200

    fix merge

--- pages/index.php
+++ pages/index.php
@@ -2,7 +2,7 @@
 require_once("../database/init.php");
 $templateParams["titolo"] = "Il Puzzo";
 $templateParams["js"] = [JAVASCRIPT_DIR."home.js"];
-$templateParams['css'] = [CSS_DIR."style.css", CSS_DIR."base.css"];
+$templateParams['css'] = [CSS_DIR."style.css"];
 if(!($dbh->isLogged() && $dbh->getUserType() == "vendor")) {
     $templateParams['js'][] = JAVASCRIPT_DIR."suggestions.js";
 }
