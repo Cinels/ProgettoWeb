5ee64ca9fe2d6703087c33bcf7f17d05016eef98
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-07-10 01:10:35 +0200

    adapt manage_product to the mockup

--- template/user/manage_product.php
+++ template/user/manage_product.php
@@ -5,17 +5,18 @@
     echo "<input type=\"hidden\" name=\"update\" value=".$templateParams['id'].">";
     endif;
     ?>
-  <label for="nome">Nome:</label><br>
-  <input type="text" id="nome" name="nome" maxlength="50" value ="<?php echo $templateParams["product"]["nome"] ?? "" ?>" required><br><br>
-
-  <label for="prezzo">Prezzo (€):</label><br>
-  <input type="number" id="prezzo" name="prezzo" step="0.01" min="0" max="9999.99" value ="<?php echo $templateParams["product"]["prezzo"] ?? "" ?>" required><br><br>
 
   <label for="image1">Immagine 1:<img src="<?php echo (isset($templateParams['id'])) ? DB_RESOURCES_DIR.$templateParams['images'][0]['link'] : ""?>" /></label><br>
   <input type="file" name="image1" id="image1" accept="image/*" <?php echo (!isset($templateParams['id']) ? "required" : "") ?>/><br><br>
 
   <label for="image2">Immagine 2:<img src="<?php echo (isset($templateParams['id'])) ? DB_RESOURCES_DIR.$templateParams['images'][1]['link'] : ""?>" /></label><br>
   <input type="file" name="image2" id="image2" accept="image/*"/><br><br>
+
+  <label for="nome">Nome:</label><br>
+  <input type="text" id="nome" name="nome" maxlength="50" value ="<?php echo $templateParams["product"]["nome"] ?? "" ?>" required><br><br>
+
+  <label for="prezzo">Prezzo (€):</label><br>
+  <input type="number" id="prezzo" name="prezzo" step="0.01" min="0" max="9999.99" value ="<?php echo $templateParams["product"]["prezzo"] ?? "" ?>" required><br><br>
 
   <label for="quantita">Quantità Disponibile:</label><br>
   <input type="number" id="quantita" name="quantitaDisponibile" min="0" value ="<?php echo $templateParams["product"]["quantitaDisponibile"] ?? "" ?>" required><br><br>
@@ -45,8 +46,10 @@
     <option value="3" <?php echo $templateParams["product"]["idPiattaforma"] == 3 ? "selected" : "" ?>>Microsoft</option>
   </select><br><br>
 
-  <button type="submit"><?php echo $templateParams["h2"] ?></button>
+  <button type="submit"><?php echo $templateParams["h2"] ?><img src="<?php echo RESOURCES_DIR."Marco_semplice_W.png"?>" alt="" name ="update" /></button>
 </form>
-
+<?php if(isset($templateParams['id'])): ?>
+ <a href="product.php?search=<?php echo $templateParams['id'] ?>"><button type="button">Annulla<img src="<?php echo RESOURCES_DIR."x.png"?>" alt="" name ="cancel"/></button><a>
+<?php endif; ?>
 
 </section>
