7efbe77c5194e77a86e745fd0b6196ee0760814a
Lorenzo Cinelli <lorenzo.cinelli.it@gmail.com>
2025-07-10 00:13:47 +0200

    Added style sheet for suggestions aside content and fixed the suggestions page

--- pages/results.php
+++ pages/results.php
@@ -2,7 +2,7 @@
 require_once("../database/init.php");
 $templateParams["titolo"] = "Risultati";
 $templateParams['js'] = [JAVASCRIPT_DIR.'results.js'];
-$templateParams['css'] = [CSS_DIR."base.css", CSS_DIR."style.css"];
+$templateParams['css'] = [CSS_DIR."base.css", CSS_DIR."aside.css", CSS_DIR."style.css"];
 if(!isset($_GET["search"])) {
     header('Location:'.PAGES_DIR."index.php");    
 }
