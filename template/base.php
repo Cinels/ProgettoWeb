384abf45b38cabc4f5e6712994ab0a4e7fd832be
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-07-10 03:39:48 +0200

    fix pro pic visualization

--- template/base.php
+++ template/base.php
@@ -21,7 +21,7 @@
                     <a href="<?php echo PAGES_DIR ?>cart.php"><img src="<?php echo RESOURCES_DIR ?>header/carrello.png" alt="Carrello"/></a>
                 <?php endif; ?>
                 <a href="<?php echo PAGES_DIR ?>orders.php"><img src="<?php echo RESOURCES_DIR ?>header/ciamioncino.png" alt="Ordini"/></a>
-                <a href="<?php echo PAGES_DIR ?>profile.php"><img src="<?php echo $dbh->getProfileImage() ?? RESOURCES_DIR."header/utente.png"; ?>" alt="Profilo"/></a>
+                <a href="<?php echo PAGES_DIR ?>profile.php"><img src="<?php echo ($dbh->getProfileImage() !== null ? DB_RESOURCES_DIR.$dbh->getProfileImage() : RESOURCES_DIR."header/utente.png")?>" alt="Profilo"/></a>
             </div>
             <!-- <form action="<?php echo PAGES_DIR ?>results.php" method="GET">
                 <img src="<?php echo RESOURCES_DIR ?>header/search.png" alt="Cerca"/>
