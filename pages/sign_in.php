10b6d1866add38f02703eea385c5950dfd804c2b
Lorenzo Cinelli <lorenzo.cinelli.it@gmail.com>
2025-07-14 20:24:30 +0200

    Modified page title

--- pages/sign_in.php
+++ pages/sign_in.php
@@ -1,6 +1,6 @@
 <?php
 require_once("../database/init.php");
-$templateParams["titolo"] = "Registrati";
+$templateParams["titolo"] = ((isset($_GET['action']) && $_GET['action'] === 'edit') ? "Modifica Profilo" : "Registrati");
 $templateParams['js'] = [JAVASCRIPT_DIR.'sign_in.js'];
 $templateParams['css'] = [CSS_DIR."base.css", CSS_DIR."main.css"];
 require("../template/base.php")
