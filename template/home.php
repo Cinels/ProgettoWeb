be0c0ad6447d00cb4e9ef0768eadf8c72ef47950
Lorenzo Cinelli <lorenzo.cinelli.it@gmail.com>
2025-07-14 22:48:28 +0200

    Removed OBSOLETE and UNUSED php template files

--- template/home.php
+++
@@ -1,5 +0,0 @@
-<section>
-    <a href="<?php echo PAGES_DIR ?>results.php?search=ps"><img src="<?php echo RESOURCES_DIR ?>playstation.png" alt="Playstation"/></a>
-    <a href="<?php echo PAGES_DIR ?>results.php?search=switch"><img src="<?php echo RESOURCES_DIR ?>switch.png" alt="Nintendo Switch"/></a>
-    <a href="<?php echo PAGES_DIR ?>results.php?search=xbox"><img src="<?php echo RESOURCES_DIR ?>xbox.png" alt="Xbox"/></a>
-</section>
