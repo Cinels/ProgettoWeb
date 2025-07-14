c483cc801c962c8c8722441c7b1180ca33087dca
Lorenzo Cinelli <lorenzo.cinelli.it@gmail.com>
2025-07-14 11:39:20 +0200

    Added media query for base and aside. Modified suggestion limit in query to 4

--- api/suggestions.php
+++ api/suggestions.php
@@ -4,13 +4,13 @@ require_once("../database/init.php");
 $result['result'] = null;
 if (isset($_GET['suggestion'])) {
     if ($_GET['suggestion'] === 'sale') {
-        $result['result'] = $dbh->getProductsOnSale(10);
+        $result['result'] = $dbh->getProductsOnSale(4);
     } elseif ($_GET['suggestion'] === 'trends') {
-        $result['result'] = $dbh->getTrendProducts(10);
+        $result['result'] = $dbh->getTrendProducts(4);
     } elseif ($_GET['suggestion'] === 'interests') {
-        $result['result'] = $dbh->getYourInterestProducts(10);
+        $result['result'] = $dbh->getYourInterestProducts(4);
     } elseif($_GET['suggestion'] === 'related' && isset($_GET['search'])) {
-        $result['result'] = $dbh->getRelatedProducts($_GET['search'], 10);
+        $result['result'] = $dbh->getRelatedProducts($_GET['search'], 4);
     }
 }
 
